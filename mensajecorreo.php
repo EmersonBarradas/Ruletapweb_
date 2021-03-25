56c0738f08c84bb30bbb3fdcc334a254de46e7e7
Emerson Barradas <30486610+EmersonBarradas@users.noreply.github.com>
2021-03-24 21:02:41 -0400

    Add files via upload

---
+++ mensajecorreo.php
@@ -0,0 +1,75 @@
+<?PHP 
+
+'<!DOCTYPE html>'.
+'<html lang="es">'.
+'<head>'.
+    '<meta charset="UTF-8">'.
+    '<meta http-equiv="X-UA-Compatible" content="IE=edge">'.
+    '<meta name="viewport" content="width=device-width, initial-scale=1.0">'.
+    '<title>Mensaje de Correo Rule Tap</title>'.
+    '<script src="https://kit.fontawesome.com/f5f2444b05.js"></script> <!- Enlace a fontawesome.com ->  '.             
+    '<style type="text/css">'.
+        '@import url(https://fonts.googleapis.com/css?family=Comfortaa:300,400,500,600,700,800,300italic,400italic,600italic,700italic,800italic);'.
+    '</style>'.
+'</head>'.
+'<body>'.
+    '<div style="width: 100%; text-align: center; background: #ffffff; margin: 0 auto;">'.
+        '<img id="logoazul" class=" margin: 0 auto;" width="120px" src="https://centrodeprueba.secorgadlara.org/img/logoazul.png" alt="Logo Ruleta Tap">'.
+    '</div>'.
+    '<div style="width: 100%;">'.
+        '<hr>'.
+    '</div>'.
+    '<div style="padding: 20px 0 0px 0; width: 100%; text-align: center; background: #ffffff; margin: 0 auto;">'.
+        '<img id="logoazul" class="  " width="100px" src="https://centrodeprueba.secorgadlara.org/img/correo/cono.png" alt="Logo Ruleta Tap">'.
+    '</div>'.
+    '<div style="padding: 0 0 0 0; text-align:center; font-family: Arial;">'.
+        '<h1 class="">¡Felicitaciones!</h1>'.
+    '</div>'.
+    '<div style="padding: 10px 0 30px 0; text-align:center; line-height: 12px; font-family: Open Sans; font-size: 16px; color:#353535;
+    ">'.
+        '<p class="font-size: 16px;">Ganaste <b>$premio</b>   para pagar tus servicios con Tap, tope <b>$montodetope</b> .</p>'.
+        '<p class="font-size: 16px;">Para usar el descuento seguí estos pasos:</p>'.
+    '</div>'.
+    '<div style="width: 100%; text-align: center; background: #ffffff; margin: 0 auto;">'.
+        '<img id="logoazul" class="  " width="270px" src="https://centrodeprueba.secorgadlara.org/img/correo/pasos.png" alt="Logo Ruleta Tap">'.
+    '</div>'.
+    '<div style="padding: 30px 0 0px 0; text-align:center; font-family: Arial, Helvetica, sans-serif; font-size: 16px; line-height: 12px;">'.
+        '<p class="font-size: 16px;">El código de descuento de tu premio es:</p>'.
+    '</div>'.
+    '<div style="padding: 0 0 40px 0; text-align:center; font-family: Arial, Helvetica, sans-serif; font-size: 16px;">'.
+        '<h2>$PROMOCODE</h2>'.
+    '</div>'.
+    '<div style="padding: 0 0 40px 0; text-align:center; font-family: Comfortaa;">'.
+        '<a href="https://auntap.com" style="background: #14D2B9; border-radius: 24px; height: 60px; width: 250px; padding: 15px 100px; color: #ffffff; text-decoration: none; font-size: 16px;">Descargá Tap</a>'.
+    '</div>'.
+    '<div style="padding: 5px 0 50px 0; text-align:center; font-family: Open Sans; font-size: 16px; line-height: 12px; color: #353535;">'.
+        '<p class="font-size: 16px;">Tap es una nueva billetera digital en Argentina en la cual vas a poder </p>'.
+        '<p class="font-size: 16px;">comprar en comercios, ahorrar, pagar los servicios, todo desde tu celular!</p>'.
+    '</div>'.
+    '<div style="padding: 20px 0 20px 0; width: 100%; text-align:center; background: #8C91FF; padding: 30px 0 30px 0; font-family: Arial; line-height: 16px;">'.
+        '<div>'.
+            '<p style="color: #ffffff; font-weight: 400; font-size: 20px;"> <b>¡Saludos!</b>  Equipo de Tap</p>'.
+        '</div>'.
+        '<div>'.
+            '<p style="padding: 0px 0 30px 0;color: #ffffff; font-weight: 400; font-size: 12px;">Si tenés dudas, conáctanos</p>'.
+        '</div>'.
+        '<div style="padding: 30px 0 30px 0; width: 100%; text-align:center;  margin: 0 auto; padding: 0px 0 0px 0;">'.
+            '<img id="" class="  " width="30px" src="https://centrodeprueba.secorgadlara.org/img/correo/mail.png" alt="Logo Ruleta Tap">'.
+            '&nbsp;&nbsp;'.
+            '<img id="" class="  " width="30px" src="https://centrodeprueba.secorgadlara.org/img/correo/mail.png" alt="Logo Ruleta Tap">'.
+            '&nbsp;&nbsp;'.
+            '<img id="" class="  " width="30px" src="https://centrodeprueba.secorgadlara.org/img/correo/facebook.png" alt="Logo Ruleta Tap">'.
+            '&nbsp;&nbsp;'.
+            '<img id="" class="  " width="30px" src="https://centrodeprueba.secorgadlara.org/img/correo/twitter.png" alt="Logo Ruleta Tap">'.
+            '&nbsp;&nbsp;'.
+            '<img id="" class="  " width="30px" src="https://centrodeprueba.secorgadlara.org/img/correo/whatsapp.png" alt="Logo Ruleta Tap">'.
+
+        '</div>'.
+        '<div>'.
+            '<p style="padding: 10px 0 0px 0; color: #ffffff; font-weight: 700; font-size: 10px;">Este es un mail generado automáticamente, porfavor no respondas a esta casilla de correo.</p>'.
+        '</div>'.
+    '</div>'.
+'</body>'.
+'</html>';
+
+?>
